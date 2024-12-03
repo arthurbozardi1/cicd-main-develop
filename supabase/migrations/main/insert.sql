5c409158aaf9e18e351a213cb04e3cc4dc24c304
arthur.bozardi <arthur.bozardi@plenatech.com>
2024-12-03 14:33:57 -0300

    ajuste

--- supabase/migrations/main/insert.sql
+++ supabase/migrations/main/insert.sql
@@ -5,16 +5,5 @@ CREATE TABLE migrations (
     migration_name VARCHAR(255) NOT NULL,
     applied_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(20) DEFAULT 'pending',
-    checksum VARCHAR(64) -- Hash do conteúdo SQL (opcional)
-);
-
-
-
-CREATE TABLE IF NOT EXISTS users (
-  id BIGSERIAL PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT NOW(),
-  updated_at TIMESTAMPTZ DEFAULT NOW()
+    checksum VARCHAR(64)
 );
