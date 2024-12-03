a38f52b42071d39654f2a0828ee00230e34f4da3
arthurbozardi1 <arthurbozardi@hotmail.com>
2024-12-03 03:28:42 -0300

    ajustes

--- supabase/migrations/develop/sasa.sql
+++ supabase/migrations/develop/sasa.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS usuarios (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL
 );
-
 CREATE INDEX IF NOT EXISTS idx_usuarios_nome ON usuarios (nome);
+
+CREATE TABLE pedra (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL
+);
