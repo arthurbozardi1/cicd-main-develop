0411f5e1beecc644da1d8eb12215e54ed306e8c6
arthurbozardi1 <arthurbozardi@hotmail.com>
2024-12-03 05:59:39 -0300

    ajustes

--- supabase/migrations/main/insert.sql
+++ supabase/migrations/main/insert.sql
@@ -1,4 +1,15 @@
 -- Criação da tabela 'users'
+
+CREATE TABLE migrations (
+    id SERIAL PRIMARY KEY,
+    migration_name VARCHAR(255) NOT NULL,
+    applied_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20) DEFAULT 'pending',
+    checksum VARCHAR(64) -- Hash do conteúdo SQL (opcional)
+);
+
+
+
 CREATE TABLE IF NOT EXISTS users (
   id BIGSERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
