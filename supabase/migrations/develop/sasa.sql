e4b9b79d7950230a76b9d3826fe19700e50d7bea
arthurbozardi1 <arthurbozardi@hotmail.com>
2024-12-03 04:25:49 -0300

    ajustes

--- supabase/migrations/develop/sasa.sql
+++ supabase/migrations/develop/sasa.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
+
+-- Migration: Create Users Table
+CREATE TABLE IF NOT EXISTS dasdsa (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
