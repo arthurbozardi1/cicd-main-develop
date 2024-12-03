28e8b94e34754e669f423202fa20e723ca8f6024
arthurbozardi1 <arthurbozardi@hotmail.com>
2024-12-03 03:50:50 -0300

    ajustes

--- supabase/migrations/develop/sasa.sql
+++ supabase/migrations/develop/sasa.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 );
 CREATE INDEX IF NOT EXISTS idx_usuarios_nome ON usuarios (nome);
 
-CREATE TABLE fsadfg  (
+CREATE TABLE asd  (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL
 );
